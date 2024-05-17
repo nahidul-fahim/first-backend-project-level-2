336ab4fd0cc555bdcf061a3636d2162588585571
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-17 21:20:21 +0600

    created interface, schema, and model:

---
+++ src/app/modules/student/student.model.ts
@@ -0,0 +1,103 @@
+import { Schema, model, connect } from 'mongoose';
+import { Guardian, LocalGuardian, Student, UserName } from './student.interface';
+
+// userName schema
+const userNameSchema = new Schema<UserName>({
+    firstName: {
+        type: String,
+        required: true
+    },
+    middleName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+})
+
+// guardian schema
+const guardianSchema = new Schema<Guardian>({
+    fatherName: {
+        type: String,
+        required: true
+    },
+    fatherOccupation: {
+        type: String,
+        required: true
+    },
+    fatherContactNo: {
+        type: String,
+        required: true
+    },
+    motherName: {
+        type: String,
+        required: true
+    },
+    motherOccupation: {
+        type: String,
+        required: true
+    },
+    motherContactNo: {
+        type: String,
+        required: true
+    },
+})
+
+// local guardian schema
+const localGuardianSchema = new Schema<LocalGuardian>({
+    name: {
+        type: String,
+        required: true,
+    },
+    occupation: {
+        type: String,
+        required: true,
+    },
+    contactNo: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+})
+
+
+
+// student schema
+const studentSchema = new Schema<Student>({
+    id: { type: String },
+    name: userNameSchema,
+    gender: ["male", "female"],
+    dateOfBirth: { type: String },
+    email: {
+        type: String,
+        required: true,
+    },
+    contactNo: {
+        type: String,
+        required: true
+    },
+    emergencyContactNo: {
+        type: String,
+        required: true
+    },
+    bloodGroup: ["A+", "B+", "O+", "AB+", "A-", "B-", "AB-", "O-"],
+    presentAddress: {
+        type: String,
+        required: true
+    },
+    permanentAddress: {
+        type: String,
+        required: true
+    },
+    guardian: guardianSchema,
+    localGuardian: localGuardianSchema,
+    profileImg: { type: String },
+    isActive: ["active", "blocked"]
+})
+
+// creating model
+const Student = model<Student>('Student', studentSchema)
