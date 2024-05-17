eefaf35bf365fcfc3e4780cded55280113bc1400
nahidul-fahim <nahidulfahim.official@gmail.com>
2024-05-17 16:23:46 +0600

    installed express, typescript, mongoose, dtoenv, cors

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express()
+const port = 3000;
+
+// parsers
+app.use(express.json())
+app.use(cors())
+
+// simple get request
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send('Hello World!')
+})
+
+
+export default app;
